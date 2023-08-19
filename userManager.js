4fc9739a9e9b04eed2d5d1f37513f2674e6a448a
Miguel Salazar <yobebe@hotmail.com.ar>
2023-08-18 21:01:36 -0300

    agregando los cambios que me pidio mi tutor

--- userManager.js
+++ userManager.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-class userManager {
+class UserManager {
     constructor(path) {
         this.path = path;
     }
@@ -15,7 +15,7 @@ class userManager {
         if (usuarios.length > 0) {
             let existe = usuarios.findIndex(usuario => usuario.email === email);
             if (existe !== -1) {
-                console.log(`Tu ${email} ya existe maquina!!`);
+                console.log(`El email ${email} ya existe.`);
                 return;
             }
         }
@@ -32,13 +32,52 @@ class userManager {
         usuarios.push(nuevoUsuario);
         await fs.promises.writeFile(this.path, JSON.stringify(usuarios, null, 5));
     }
+    async getProductById(id) {
+        const usuarios = await this.getUsers();
+        return usuarios.find(usuario => usuario.id === id);
+    }
+    async updateProduct(id, nombre, apellido, email) {
+        let usuarios = await this.getUsers();
+        const usuarioIndex = usuarios.findIndex(usuario => usuario.id === id);
+        if (usuarioIndex !== -1) {
+            usuarios[usuarioIndex] = {
+                id,
+                nombre,
+                apellido,
+                email
+            };
+            await fs.promises.writeFile(this.path, JSON.stringify(usuarios, null, 5));
+        } else {
+            console.log(`El usuario con ID ${id} no existe.`);
+        }
+    }
+    async deleteProduct(id) {
+        let usuarios = await this.getUsers();
+        const usuarioIndex = usuarios.findIndex(usuario => usuario.id === id);
+        if (usuarioIndex !== -1) {
+            usuarios.splice(usuarioIndex, 1);
+            await fs.promises.writeFile(this.path, JSON.stringify(usuarios, null, 5));
+        } else {
+            console.log(`El usuario con ID ${id} no existe.`);
+        }
+    }
 }
 const entorno = async () => {
     let path = "./user.json";
-    let manager = new userManager(path);
+    let manager = new UserManager(path);
+    
     await manager.addUser('nombre1', 'apellido1', 'email1@example.com');
     await manager.addUser('nombre2', 'apellido2', 'email2@example.com');
     await manager.addUser('nombre3', 'apellido3', 'email3@example.com');
+    
+    console.log(await manager.getUsers());
+    const usuario = await manager.getProductById(2);
+    console.log("Usuario con ID 2:", usuario);
+    await manager.updateProduct(2, 'nombre_modificado', 'apellido_modificado', 'email2_modificado@example.com');
+    console.log("Usuarios después de la actualización:");
+    console.log(await manager.getUsers());
+    await manager.deleteProduct(1);
+    console.log("Usuarios después de la eliminación:");
     console.log(await manager.getUsers());
 };
 entorno();
